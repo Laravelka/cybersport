2cc02f2eb3ee29f6f323ff1a44911b65a12eebae
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2021-09-16 13:41:03 +0200

    added data validation on store and update User

---
+++ app/Http/Requests/UserStoreRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:255',
+            'email' => 'required|unique:users|email|max:255',
+            'phone' => 'required|unique:users|max:255',
+            'password' => 'required|max:255',
+            'first_name' => 'nullable|max:255',
+            'last_name' => 'nullable|max:255',
+            'telegram' => 'nullable|max:255',
+            'discord' => 'nullable|max:255',
+            'avatar' => 'nullable|max:255',
+        ];
+    }
+}
