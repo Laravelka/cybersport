2c9c87a980f86a668c0bbe058caac1fd5d3088fb
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2021-09-27 10:43:38 +0200

    added comment store and show functionality

---
+++ app/Http/Controllers/Api/v1/CommentController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CommentStoreRequest;
+use App\Http\Resources\CommentResource;
+use App\Models\Comment;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CommentStoreRequest $request)
+    {
+        $request_data = array_diff($request->validated(), [null]);
+
+        $request_data['user_id'] = Auth::id();
+
+        if ($request->hasFile('img')) {
+            $path = $request->img->store('comments', 'public');
+            $request_data['img'] = $path;
+        }
+
+        $comment = Comment::create($request_data);
+
+        return new CommentResource($comment);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return new CommentResource(Comment::findOrFail($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
