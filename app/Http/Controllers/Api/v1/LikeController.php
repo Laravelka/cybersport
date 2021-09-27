da8d010d4de865c38777859f743c5b2287de5836
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2021-09-27 12:21:44 +0200

    added like store functionality

---
+++ app/Http/Controllers/Api/v1/LikeController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LikeStoreRequest;
+use App\Http\Resources\LikeResource;
+use App\Models\Like;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LikeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(LikeStoreRequest $request)
+    {
+        $request_data = array_diff($request->validated(), [null]);
+
+        $request_data['user_id'] = Auth::id();
+
+        if (isset($request_data['post_id'])) {
+            if (!$this->postHasLike($request_data['post_id'], $request_data['user_id'])) {
+                $like = Like::create($request_data);
+
+                return new LikeResource($like);
+            } else {
+                return response()->json([
+                    'message' => "You already liked this post"
+                ], 422);
+            }
+        } else {
+            return response()->json([
+                'message' => 'Post not found'
+            ], 404);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function postHasLike($post_id, $user_id)
+    {
+        return (
+            Like::where('post_id', $post_id)
+                ->where('user_id', $user_id)
+                ->first()
+        ) ? true : false;
+    }
+}
