77aa2d71c6fdf8ffec61f65452b5844e4b84ee09
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2022-02-01 10:28:50 +0100

    separated Chat component from Matches

---
+++ resources/js/components/chat/Chat.vue
@@ -0,0 +1,99 @@
+<template>
+        <div class="match-chat">
+            <div class="match-chat__inner">
+                <form class="match-chat__form" action="#">
+                    <div class="match-chat__label-box">
+                        <label class="match-chat__label">
+                            <a class="match-chat__icon" href="#">
+                                <div class="match-chat__icongradient icon-chat">
+                                    <img class="match-chat__iconimg" src="images/user/1.png" alt="">
+                                </div>
+                            </a>
+                            <div class="match-chat__text-inner">
+                                <div class="match-chat__text-username chat-username">Nickname</div>
+                                <div class="match-chat__message-box">
+                                    <div class="match-chat__message">Данное сообщение с кучей текста от болельшика.</div>
+                                    <div class="match-chat__message-time">20:47</div>
+                                </div>
+                            </div>
+                        </label>
+                        <label class="match-chat__label">
+                            <a class="match-chat__icon" href="#">
+                                <div class="match-chat__icongradient icon-chat">
+                                    <img class="match-chat__iconimg" src="images/user/1.png" alt="">
+                                </div>
+                            </a>
+                            <div class="match-chat__text-inner">
+                                <div class="match-chat__text-username chat-username">Nickname</div>
+                                <div class="match-chat__message-box">
+                                    <div class="match-chat__message">Данное сообщение с кучей текста от болельшика.</div>
+                                    <div class="match-chat__message-time">20:47</div>
+                                </div>
+                            </div>
+                        </label>
+                        <label class="match-chat__label">
+                            <a class="match-chat__icon" href="#">
+                                <div class="match-chat__icongradient icon-chat">
+                                    <img class="match-chat__iconimg" src="images/user/1.png" alt="">
+                                </div>
+                            </a>
+                            <div class="match-chat__text-inner">
+                                <div class="match-chat__text-username chat-username">Nickname</div>
+                                <div class="match-chat__message-box">
+                                    <div class="match-chat__message">Данное сообщение с кучей текста от болельшика.</div>
+                                    <div class="match-chat__message-time">20:47</div>
+                                </div>
+                            </div>
+                        </label>
+                        <label class="match-chat__label">
+                            <a class="match-chat__icon" href="#">
+                                <div class="match-chat__icongradient icon-chat">
+                                    <img class="match-chat__iconimg" src="images/user/1.png" alt="">
+                                </div>
+                            </a>
+                            <div class="match-chat__text-inner">
+                                <div class="match-chat__text-username chat-username">Nickname</div>
+                                <div class="match-chat__message-box">
+                                    <div class="match-chat__message">Данное сообщение с кучей текста от болельшика.</div>
+                                    <div class="match-chat__message-time">20:47</div>
+                                </div>
+                            </div>
+                        </label>
+                        <label class="match-chat__label">
+                            <a class="match-chat__icon" href="#">
+                                <div class="match-chat__icongradient icon-chat">
+                                    <img class="match-chat__iconimg" src="images/user/1.png" alt="">
+                                </div>
+                            </a>
+                            <div class="match-chat__text-inner">
+                                <div class="match-chat__text-username chat-username">Nickname</div>
+                                <div class="match-chat__message-box">
+                                    <div class="match-chat__message">Данное сообщение с кучей текста от болельшика.</div>
+                                    <div class="match-chat__message-time">20:47</div>
+                                </div>
+                            </div>
+                        </label>
+                    </div>
+                    <div class="match-chat__input-box">
+                        <input class="match-chat__form-input" placeholder="Сообщение..." type="text">
+                        <button class="match-chat__btn-attach">
+                            <img src="images/icons/attach.svg" alt="">
+                        </button>
+                        <button class="match-chat__btn-emoji">
+                            <img src="images/icons/emoji.svg" alt="">
+                        </button>
+                        <button class="match-chat__btn-send">
+                            <img src="images/icons/send.svg" alt="">
+                        </button>
+                    </div>
+                </form>
+                <button class="match-chat__btn">
+                    <img class="match-chat__btn-img" src="images/icons/arrow-right.svg" alt="">
+                </button>
+            </div>
+        </div>
+</template>
+
+<script>
+
+</script>
