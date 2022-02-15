c9c58f1f695ec96cf0d41515e7e6ca74408fad1a
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2022-02-15 15:14:29 +0100

    added Login component and route

---
+++ resources/js/components/auth/Login.vue
@@ -0,0 +1,68 @@
+<template>
+        <div class="login popup-bg">
+            <div class="login-inner">
+                <div class="login-item login-item_gradient">
+                    <router-link
+                            :to="{ name: 'home' }"
+                            class="login-item__arrow btn-arrow">
+                        <img src="images/icons/arrow-left.svg" alt="">
+                    </router-link>
+                    <img class="login-item__img" src="images/logo.svg" alt="">
+                    <h5 class="login-item__title popup-title">Добро пожаловать</h5>
+                    <form class="login-item__form" @submit.prevent action="#">
+                        <fieldset class="login-item__fieldset-input fieldset-input">
+                            <legend>Введите почту или телефон</legend>
+                            <input class="login-item__form-input input-login" placeholder="USER322@mail.ru" type="text">
+                        </fieldset>
+                        <fieldset class="login-item__fieldset-input fieldset-input">
+                            <legend>Введите пароль</legend>
+                            <input class="login-item__form-input input-login" placeholder="********" type="password">
+                        </fieldset>
+                        <button class="login-item__form-btn btn-login">Войти</button>
+                    </form>
+                    <div class="login-item__info-box">
+                        <a class="login-item__restore" href="#">Забыли пароль?</a>
+                        <div class="login-item__signup">У вас еще нет аккаунта?
+                            <a href="#">Зарегистрироваться</a>
+                        </div>
+                        <div class="login-item__social">Войти через:</div>
+                        <ul class="login-item__social-list">
+                            <li class="login-item__social-item">
+                                <a class="login-item__social-link" href="#">
+                                    <img class="login-item__social-linkimg" src="images/icons/vk.svg" alt="">
+                                </a>
+                            </li>
+                            <li class="login-item__social-item">
+                                <a class="login-item__social-link" href="#">
+                                    <img class="login-item__social-linkimg" src="images/icons/yandex.svg" alt="">
+                                </a>
+                            </li>
+                            <li class="login-item__social-item">
+                                <a class="login-item__social-link" href="#">
+                                    <img class="login-item__social-linkimg" src="images/icons/google.svg" alt="">
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+</template>
+
+<script>
+    export default {
+        components: {
+
+        },
+        data() {
+            return {
+
+            }
+        },
+        computed: {
+            backLink() {
+                return this.$router.back();
+            }
+        }
+    }
+</script>
