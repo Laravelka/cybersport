fb481d3ad5205bf5c48100ffd137ea392cdade99
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2022-02-18 18:03:26 +0100

    added common store module with error and loading data, added Loader component

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -1,10 +1,12 @@
 import {createStore} from "vuex";
+import {commonModule} from "./common";
 import {currentUserModule} from "./currentUserModule";
 import {matchesModule} from "./matchesModule";
 import {chatMessagesModule} from "./chatMessagesModule";
 
 export default createStore({
     modules: {
+        common: commonModule,
         currentUser: currentUserModule,
         matches: matchesModule,
         messages: chatMessagesModule
